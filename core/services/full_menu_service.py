2d795a75a37e194681d27635e4b0cbb9ee42d195
yakhl <suflexer@gmail.com>
2023-08-18 01:20:45 +0300

    fix return full menu cache

--- core/services/full_menu_service.py
+++ core/services/full_menu_service.py
@@ -7,7 +7,9 @@ from core.schemas.full_menu_schema import Menu
 
 class FullMenuService:
     def __init__(
-        self, cache_repository: FullMenuCacheRepository = Depends(), full_menu_repository: FullMenuRepository = Depends()
+        self,
+        cache_repository: FullMenuCacheRepository = Depends(),
+        full_menu_repository: FullMenuRepository = Depends(),
     ):
         self.full_menu_repository = full_menu_repository
         self.cache_repository = cache_repository
@@ -15,7 +17,7 @@ class FullMenuService:
     async def get(self) -> list[Menu]:
         cached_full_menu = await self.cache_repository.get_full_menu()
         if cached_full_menu:
-            cached_full_menu
+            return cached_full_menu
         db_full_menu = await self.full_menu_repository.get()
         await self.cache_repository.set_full_menu(db_full_menu)
         return db_full_menu
